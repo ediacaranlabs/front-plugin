feb42b0db3000bf1a4670f7c5e7cabc162c22a5e
Afonso <afonso.rbn@gmail.com>
2019-05-27 10:58:55 -0300

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/BoxTag.java
@@ -0,0 +1,83 @@
+package br.com.uoutec.community.ediacaran.front.tags;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;
+
+public class BoxTag extends EdiacaranSimpleTagSupport {
+
+	public static final String TEMPLATE             = "bootstrap4/templates/components/box";
+	
+	@SuppressWarnings("serial")
+	private static final Set<String> ignore = new HashSet<String>() {{
+		add("class");
+	}};
+	
+	private String icon;
+	
+	private String title;
+	
+	private String lnk;
+	
+	private String btn;
+	
+	public BoxTag() {
+	}
+	
+    public void doTag() throws JspException, IOException{
+    	
+    	try {
+			Map<String, Object> vars = new HashMap<String, Object>();
+			vars.put("attr",    super.toAttrs(null, ignore));
+			vars.put("icon",    icon);
+			vars.put("title",   title);
+			vars.put("lnk",     lnk);
+			vars.put("btn",     btn);
+			vars.put("content", new JspFragmentVarParser(getJspBody()));
+			
+			TemplatesManager.getTemplatesManager()
+				.apply(this.getTemplate() == null? TEMPLATE : this.getTemplate(), vars, getJspContext().getOut());
+    	}
+    	catch(Throwable e) {
+    		throw new IllegalStateException(e);
+    	}
+    	
+    }
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLnk() {
+		return lnk;
+	}
+
+	public void setLnk(String lnk) {
+		this.lnk = lnk;
+	}
+
+	public String getBtn() {
+		return btn;
+	}
+
+	public void setBtn(String btn) {
+		this.btn = btn;
+	}
+	
+}
---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/IconTag.java
@@ -0,0 +1,107 @@
+package br.com.uoutec.community.ediacaran.front.tags;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;
+
+public class IconTag extends EdiacaranSimpleTagSupport {
+
+	public static final String TEMPLATE       = "bootstrap4/templates/components/icon";
+	
+	public static final String TEMPLATE_STACK = "bootstrap4/templates/components/icon-stack";
+	
+	@SuppressWarnings("serial")
+	private static final Set<String> ignore = new HashSet<String>() {{
+		add("class");
+	}};
+	
+	private Integer size;
+	
+	private Integer bgSize;
+	
+	private Integer iconSize;
+	
+	private String bg;
+	
+	private String name;
+	
+	public IconTag() {
+	}
+	
+    public void doTag() throws JspException, IOException{
+    	
+    	try {
+			size     = size == null? 3 : size;
+			bgSize   = bgSize == null? size - 1 : bgSize;
+			iconSize = iconSize == null? bgSize - 1 : iconSize;
+			
+			Map<String, Object> vars = new HashMap<String, Object>();
+			
+			vars.put("size", size);
+			vars.put("bg-size", bgSize);
+			vars.put("bg", bg);
+			vars.put("icon", name);
+			vars.put("icon-size", iconSize);
+			vars.put("attr", super.toAttrs(null, ignore));
+			vars.put("class", this.getClassStyle());
+			
+			TemplatesManager.getTemplatesManager()
+				.apply(
+					this.getTemplate() == null? 
+							bg != null? TEMPLATE_STACK : TEMPLATE : 
+							this.getTemplate(), 
+					vars, 
+					getJspContext().getOut()
+				);
+    	}
+    	catch(Throwable e) {
+    		throw new IllegalStateException(e);
+    	}
+    	
+    }
+
+	public Integer getSize() {
+		return size;
+	}
+
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+
+	public Integer getBgSize() {
+		return bgSize;
+	}
+
+	public void setBgSize(Integer bgSize) {
+		this.bgSize = bgSize;
+	}
+
+	public Integer getIconSize() {
+		return iconSize;
+	}
+
+	public void setIconSize(Integer iconSize) {
+		this.iconSize = iconSize;
+	}
+
+	public String getBg() {
+		return bg;
+	}
+
+	public void setBg(String bg) {
+		this.bg = bg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
