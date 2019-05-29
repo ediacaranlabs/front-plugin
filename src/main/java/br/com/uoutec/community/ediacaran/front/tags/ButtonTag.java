85ad2f67cdc2440ac32da1e0b96826212c7c14e6
Afonso <afonso.rbn@gmail.com>
2019-05-28 21:38:27 -0300

    #

--- src/main/java/br/com/uoutec/community/ediacaran/front/tags/ButtonTag.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/ButtonTag.java
@@ -10,14 +10,26 @@ import javax.servlet.jsp.JspException;
 
 public class ButtonTag extends EdiacaranSimpleTagSupport {
 
-	public static final String TEMPLATE             = "bootstrap4/templates/components/button";
+	public static final String TEMPLATE = "bootstrap4/templates/components/button";
 	
 	@SuppressWarnings("serial")
 	private static final Set<String> empty = new HashSet<String>() {{
-		add("class");
+		add("classStyle");
+		add("size");
+		add("type");
+		add("block");
+		add("enabled");
 	}};
 	
-	private String title;
+	private String label;
+	
+	private String size;
+	
+	private String type;
+	
+	private Boolean block;
+	
+	private Boolean enabled;
 	
 	public ButtonTag() {
 	}
@@ -30,10 +42,13 @@ public class ButtonTag extends EdiacaranSimpleTagSupport {
     	
     	try {
 			Map<String, Object> vars = new HashMap<String, Object>();
-			vars.put("attr",  super.toAttrs());
-			vars.put("title", title);
-			vars.put("class", super.getClassStyle());
-			vars.put("path",  new JspFragmentVarParser(getJspBody()));
+			vars.put("size",    size == null               ? ""            : new String(" btn-").concat(size));
+			vars.put("type",    type == null               ? "btn-primary" : new String(" btn-").concat(type));
+			vars.put("block",   block == null              ? ""            : block? " btn-block" : ""        );
+			vars.put("enabled", enabled != null && !enabled? " disabled"    : ""                             );
+			vars.put("attr",    super.toAttrs()                                                              );
+			vars.put("class",   super.getClassStyle()                                                        );
+			vars.put("label",   label                                                                        );
 			
 			TemplatesManager.getTemplatesManager()
 				.apply(this.getTemplate() == null? TEMPLATE : this.getTemplate(), vars, getJspContext().getOut());
@@ -44,12 +59,36 @@ public class ButtonTag extends EdiacaranSimpleTagSupport {
     	
     }
 
-	public String getTitle() {
-		return title;
+	public String getSize() {
+		return size;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Boolean getBlock() {
+		return block;
+	}
+
+	public void setBlock(Boolean block) {
+		this.block = block;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
 	}
 
 }
