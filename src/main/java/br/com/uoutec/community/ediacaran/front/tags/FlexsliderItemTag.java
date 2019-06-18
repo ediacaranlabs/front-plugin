12a5df9e26ab9c3977ff14785ea5079a9cbe61b1
Afonso <afonso.rbn@gmail.com>
2019-06-17 22:30:28 -0300

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/FlexsliderItemTag.java
@@ -0,0 +1,80 @@
+package br.com.uoutec.community.ediacaran.front.tags;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;
+
+public class FlexsliderItemTag  extends AbstractTag {
+
+	public static final String TEMPLATE  = "/bootstrap4/templates/components/flexslider-item";
+	
+	@SuppressWarnings("serial")
+	protected static final Set<String> DEFAULT_ATTRS = 
+		Collections.unmodifiableSet(new HashSet<String>(AbstractTag.DEFAULT_ATTRS) {{
+			//add("");
+		}});
+	
+	@SuppressWarnings("serial")
+	protected static final Map<String, AttributeParser> ATTRIBUTE_PARSERS = 
+		Collections.unmodifiableMap(new HashMap<String, AttributeParser>(AbstractTag.DEFAULT_ATTRIBUTE_PARSERS){{
+			/*
+			put("", new AttributeParserImp() {
+				
+				@Override
+				public String toName(String value) {
+					return value;
+				}
+				
+				@Override
+				public Object toValue(Object value) {
+					return value;
+				}
+				
+			});
+			*/
+			
+		}});
+
+	/* ------------ Attr ---------------*/
+	
+	/* ------------ Prop ---------------*/
+	
+	private String src;
+	
+	public FlexsliderItemTag() {
+	}
+	
+    public void doInnerTag() throws JspException, IOException{
+    	
+    	try {
+			Map<String, Object> vars = new HashMap<String, Object>();
+			vars.put("src", src);
+			
+			TemplatesManager.getTemplatesManager()
+				.apply(
+						this.getTemplate() == null? 
+								TEMPLATE : 
+								this.getTemplate(), 
+						vars, getJspContext().getOut()
+				);
+    	}
+    	catch(Throwable e) {
+    		throw new IllegalStateException(e);
+    	}
+    	
+    }
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	
+}
