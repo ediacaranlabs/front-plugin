9998846fa66cfad3d7daecfd8d1caac376311556
Afonso <afonso.rbn@gmail.com>
2019-07-02 07:29:44 -0300

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/IntroTag.java
@@ -0,0 +1,89 @@
+package br.com.uoutec.community.ediacaran.front.tags;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class IntroTag  extends AbstractTag {
+
+	public static final String TEMPLATE  = "/bootstrap4/templates/components/intro";
+	
+	@SuppressWarnings("serial")
+	protected static final Set<String> DEFAULT_ATTRS = 
+		Collections.unmodifiableSet(new HashSet<String>(AbstractTag.DEFAULT_ATTRS) {{
+		}});
+	
+	@SuppressWarnings("serial")
+	protected static final Map<String, AttributeParser> ATTRIBUTE_PARSERS = 
+		Collections.unmodifiableMap(new HashMap<String, AttributeParser>(AbstractTag.DEFAULT_ATTRIBUTE_PARSERS){{
+		}});
+
+	@SuppressWarnings("serial")
+	protected static final Set<String> DEFAULT_PROPS = 
+		Collections.unmodifiableSet(new HashSet<String>(AbstractTag.DEFAULT_PROPS) {{
+			add("bgImage");
+		}});
+	
+	@SuppressWarnings("serial")
+	protected static final Map<String, AttributeParser> DEFAULT_PROPERTY_PARSERS = 
+			Collections.unmodifiableMap(new HashMap<String, AttributeParser>(AbstractTag.DEFAULT_PROPERTY_PARSERS){{
+				put("bgImage", new AttributeParserImp() {
+					
+					@Override
+					public Object toValue(Object value) {
+						return "background: url(" + value + ") no-repeat center center; background-size: cover;";
+					}
+					
+				});
+			}});
+	
+	/* ------------ Attr ---------------*/
+	
+	/* ------------ Prop ---------------*/
+	
+	private String bgImage;
+	
+	public IntroTag() {
+	}
+	
+	public Map<String, Object> getValues() {
+		Map<String, Object> vals = super.getValues();
+		vals.put("content", new JspFragmentVarParser(getJspBody()));
+		return vals;
+	}
+	
+    protected String getDefaultTemplate() {
+    	return TEMPLATE;
+    }
+
+    protected Set<String> getDefaultAttributes(){
+    	return DEFAULT_ATTRS;
+    }
+
+    protected Set<String> getEmptyAttributes(){
+    	return DEFAULT_EMPTY_ATTRIBUTES;
+    }
+    
+    protected Map<String, AttributeParser> getAttributeParsers(){
+    	return DEFAULT_ATTRIBUTE_PARSERS;
+    }
+
+    protected Set<String> getDefaultProperties(){
+    	return DEFAULT_PROPS;
+    }
+
+    protected Map<String, AttributeParser> getPropertyParsers(){
+    	return DEFAULT_PROPERTY_PARSERS;
+    }
+
+	public String getBgImage() {
+		return bgImage;
+	}
+
+	public void setBgImage(String bgImage) {
+		this.bgImage = bgImage;
+	}
+    
+}
