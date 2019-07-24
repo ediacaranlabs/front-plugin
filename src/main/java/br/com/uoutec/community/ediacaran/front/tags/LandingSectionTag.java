ad57b9ad55d430fbcbaa18cf5410e39b8a88dbe2
Afonso <afonso.rbn@gmail.com>
2019-07-24 07:22:07 -0300

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/LandingSectionTag.java
@@ -0,0 +1,117 @@
+package br.com.uoutec.community.ediacaran.front.tags;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LandingSectionTag  extends AbstractTag {
+
+	public static final String TEMPLATE  = "/bootstrap4/templates/components/landing-section";
+	
+	@SuppressWarnings("serial")
+	protected static final Set<String> DEFAULT_ATTRS = 
+		Collections.unmodifiableSet(new HashSet<String>(AbstractTag.DEFAULT_ATTRS) {{
+		}});
+	
+	@SuppressWarnings("serial")
+	protected static final Map<String, AttributeParser> ATTRIBUTE_PARSERS = 
+		Collections.unmodifiableMap(new HashMap<String, AttributeParser>(AbstractTag.DEFAULT_ATTRIBUTE_PARSERS){{
+		}});
+
+	@SuppressWarnings("serial")
+	protected static final Set<String> DEFAULT_PROPS = 
+		Collections.unmodifiableSet(new HashSet<String>(AbstractTag.DEFAULT_PROPS) {{
+			add("name");
+			add("title");
+			add("img");
+			//add("align"); align é align-left e align-right
+		}});
+	
+	@SuppressWarnings("serial")
+	protected static final Map<String, AttributeParser> DEFAULT_PROPERTY_PARSERS = 
+			Collections.unmodifiableMap(new HashMap<String, AttributeParser>(AbstractTag.DEFAULT_PROPERTY_PARSERS){{
+			}});
+	
+	/* ------------ Attr ---------------*/
+	
+	/* ------------ Prop ---------------*/
+	
+	private String name;
+	
+	private String title;
+	
+	private String img;
+	
+	private String align;
+	
+	public LandingSectionTag() {
+	}
+	
+	public Map<String, Object> getValues() {
+		Map<String, Object> vals = super.getValues();
+		vals.put("align-left",  "right".equals(align)? " col-lg-offset-1 col-sm-push-6" : "" );
+		vals.put("align-right", "right".equals(align)? " col-lg-offset-2" : "col-sm-pull-6" );
+		vals.put("content", new JspFragmentVarParser(getJspBody()));
+		return vals;
+	}
+	
+    protected String getDefaultTemplate() {
+    	return TEMPLATE;
+    }
+
+    protected Set<String> getDefaultAttributes(){
+    	return DEFAULT_ATTRS;
+    }
+
+    protected Set<String> getEmptyAttributes(){
+    	return DEFAULT_EMPTY_ATTRIBUTES;
+    }
+    
+    protected Map<String, AttributeParser> getAttributeParsers(){
+    	return DEFAULT_ATTRIBUTE_PARSERS;
+    }
+
+    protected Set<String> getDefaultProperties(){
+    	return DEFAULT_PROPS;
+    }
+
+    protected Map<String, AttributeParser> getPropertyParsers(){
+    	return DEFAULT_PROPERTY_PARSERS;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getAlign() {
+		return align;
+	}
+
+	public void setAlign(String align) {
+		this.align = align;
+	}
+
+
+}
