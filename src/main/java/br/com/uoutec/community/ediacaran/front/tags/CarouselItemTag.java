b33892cce0e5ecf021d990fb0ac52dbd73a63ea2
Afonso <afonso.rbn@gmail.com>
2019-10-20 22:33:45 -0200

    #

--- src/main/java/br/com/uoutec/community/ediacaran/front/tags/CarouselItemTag.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/CarouselItemTag.java
@@ -1,10 +1,13 @@
 package br.com.uoutec.community.ediacaran.front.tags;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;;
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;;
 
 public class CarouselItemTag extends AbstractSimpleTag {
 
@@ -23,8 +26,7 @@ public class CarouselItemTag extends AbstractSimpleTag {
 	@SuppressWarnings("serial")
 	protected static final Set<String> DEFAULT_PROPS = 
 		Collections.unmodifiableSet(new HashSet<String>(AbstractSimpleTag.DEFAULT_PROPS) {{
-			add("lnk");
-			add("img");
+			add("content");
 		}});
 	
 	@SuppressWarnings("serial")
@@ -36,9 +38,12 @@ public class CarouselItemTag extends AbstractSimpleTag {
 	
 	/* ------------ Prop ---------------*/
 	
-	private String lnk;
+	private JspFragmentVarParser content;
 	
-	private String img;
+	public void doTag() throws JspException, IOException {
+		this.content = new JspFragmentVarParser(getJspBody());
+		super.doTag();
+	}
 	
     protected String getDefaultTemplate() {
     	return TEMPLATE;
@@ -63,21 +68,13 @@ public class CarouselItemTag extends AbstractSimpleTag {
     protected Map<String, AttributeParser> getPropertyParsers(){
     	return DEFAULT_PROPERTY_PARSERS;
     }
+
+	public JspFragmentVarParser getContent() {
+		return content;
+	}
+
+	public void setContent(JspFragmentVarParser content) {
+		this.content = content;
+	}
     
-	public String getLnk() {
-		return lnk;
-	}
-
-	public void setLnk(String lnk) {
-		this.lnk = lnk;
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-
 }
