4604fee29defbbfc8ef95c28eceac713a22f7592
Afonso <afonso.rbn@gmail.com>
2021-07-31 10:33:04 -0300

    #

--- src/test/java/br/com/uoutec/community/ediacaran/front/ServerBootstrapTest.java
+++ src/test/java/br/com/uoutec/community/ediacaran/front/ServerBootstrapTest.java
@@ -1,24 +1,21 @@
 package br.com.uoutec.community.ediacaran.front;
 
-import java.beans.XMLDecoder;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 import br.com.uoutec.application.se.ApplicationBoot;
 import br.com.uoutec.application.se.ApplicationBootstrap;
+import br.com.uoutec.application.se.ApplicationBootstrapProxy;
 
 public class ServerBootstrapTest {
 	
 	public static void main(String[] s) throws IOException, URISyntaxException {
 		
-		File cfg = new File("config/ediacaran-config.xml").getCanonicalFile();
-		XMLDecoder xml = new XMLDecoder(new FileInputStream(cfg));
-		ApplicationBootstrap applicationBootstrap  = (ApplicationBootstrap) xml.readObject();
-		xml.close();
+		ApplicationBootstrap applicationBootstrap  = 
+				new ApplicationBootstrapProxy("app");
 
 		ApplicationBoot.run(applicationBootstrap, new String[] {
+				"--app=config/ediacaran-config.xml",
 				"--default=config/ediacaran-dev.properties",
 				"--logger=config/log4j.configuration"
 		});
