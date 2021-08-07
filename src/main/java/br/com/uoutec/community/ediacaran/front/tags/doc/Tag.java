054f95792fc1d1e1a05f82f78a4cd3db30b708da
Afonso <afonso.rbn@gmail.com>
2021-08-07 18:04:19 -0300

    #

--- src/main/java/br/com/uoutec/community/ediacaran/front/tags/doc/Tag.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/doc/Tag.java
@@ -1,5 +1,12 @@
 package br.com.uoutec.community.ediacaran.front.tags.doc;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
 public @interface Tag {
 
 	String uri();
--- src/main/java/br/com/uoutec/community/ediacaran/front/tags/doc/TagDoc.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/doc/TagDoc.java
@@ -1,5 +1,12 @@
 package br.com.uoutec.community.ediacaran.front.tags.doc;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE , ElementType.METHOD})
 public @interface TagDoc {
 
 	String[] value();
