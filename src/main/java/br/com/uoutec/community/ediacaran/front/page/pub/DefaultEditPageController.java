e9e828d36977b4bd76abc64cd68893a3b2594bea
pc-user <pc-user@git.uoutec.com.br>
2023-04-08 16:30:43 -0300

    #

--- src/main/java/br/com/uoutec/community/ediacaran/front/page/pub/DefaultEditPageController.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/page/pub/DefaultEditPageController.java
@@ -1,19 +1,15 @@
 package br.com.uoutec.community.ediacaran.front.page.pub;
 
-import java.io.CharArrayReader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import org.brandao.brutos.annotation.Action;
 import org.brandao.brutos.annotation.Controller;
 import org.brandao.brutos.annotation.DefaultThrowSafe;
+import org.brandao.brutos.annotation.DetachedName;
 import org.brandao.brutos.annotation.MappingTypes;
 import org.brandao.brutos.annotation.Result;
 import org.brandao.brutos.annotation.ThrowSafe;
@@ -23,12 +19,9 @@ import org.brandao.brutos.annotation.web.RequestMethod;
 import org.brandao.brutos.annotation.web.RequestMethodTypes;
 import org.brandao.brutos.validator.ValidatorException;
 
-import br.com.uoutec.community.ediacaran.front.components.Image;
 import br.com.uoutec.community.ediacaran.front.objects.ObjectsManager.ObjectMetadata;
-import br.com.uoutec.community.ediacaran.front.page.BreadcrumbPath;
 import br.com.uoutec.community.ediacaran.front.page.EditPage;
 import br.com.uoutec.community.ediacaran.front.page.Page;
-import br.com.uoutec.community.ediacaran.front.page.PageFileManagerHandler;
 import br.com.uoutec.pub.entity.InvalidRequestException;
 
 @Singleton
@@ -45,81 +38,33 @@ public class DefaultEditPageController {
 	@View(value="/pages/admin/save-result")
 	@ThrowSafe( rendered=true, target=ValidatorException.class, view="/pages/admin/validation-exception")
 	@Result(value="id", mappingType=MappingTypes.VALUE)
-	public Map<String,Object> save(
-			Long gid,
-			@NotNull
-			@Size(max = 600)
-			@Pattern(regexp="(\\/|" + PageFileManagerHandler.PATH_FORMAT + ")")
-			//@Pattern(regexp=PageFileManagerHandler.PATH_FORMAT)
-			String path,
-			@Size(max = 255)
-			@Pattern(regexp=PageFileManagerHandler.ID_FORMAT)
-			String name,
-			@Pattern(regexp=PageFileManagerHandler.LOCALE_FORMAT)
-			String locale,
-			@NotNull
-			@Size(max = 255)
-			String title,
-			@Size(max = 2097152)
-			String content,
-			Map<String, String> header,
-			List<BreadcrumbPath> breadcrumb,
-			Image thumbnail,
-			@Size(max = 255)
-			String titleThumbnail,
-			@Size(max = 255)
-			String shortDescription,
-			@NotNull
-			@Size(max = 120)
-			String template) throws InvalidRequestException, ValidatorException{
+	public Map<String,Object> save(@DetachedName PagePubEntity pageEntity) throws InvalidRequestException, ValidatorException{
 
 		try {
-			Page page = new Page();
-			page.setBreadcrumb(breadcrumb);
-			page.setHeader(header);
-			page.setThumbnailDescription(shortDescription);
-			page.setThumbnailTitle(titleThumbnail);
-			page.setThumbnail(thumbnail == null? null : thumbnail.save(560, 292));
-			page.setTemplate(template);
-			page.setTitle(title);
-			page.setContent(content == null? null : new CharArrayReader(content.toCharArray()));
+			Page page = pageEntity.rebuild(pageEntity.getGid() != null, true, true);
+
+			ObjectMetadata omd;
 			
-			if(gid == null) {
-
-				if(!editPage.isValidTemplate(template)) {
-					throw new InvalidRequestException("invalid template");
-				}
-				
-				ObjectMetadata omd = name == null? 
-						editPage.registerPageByTitle(path, title, locale, page) :
-						editPage.registerPageByName(path, name, locale, page);
-
-				Map<String,Object> md = new HashMap<String,Object>();
-				md.put("path", omd.getPathMetadata().getPath());
-				md.put("id", omd.getPathMetadata().getId());
-				md.put("locale", omd.getLocale());
-
-				return md;
+			if(pageEntity.getName() != null) {
+				omd = editPage.registerPageByName(pageEntity.getPath(), 
+						pageEntity.getName(), pageEntity.getLocale(), page);
 			}
 			else {
-				
-				Map<String,Object> md = new HashMap<String,Object>();
-				md.put("path", path);
-				md.put("id", name);
-				md.put("locale", locale);
-				
-				if(gid != md.hashCode()) {
-					throw new InvalidRequestException("invalid id");
-				}
-				
-				editPage.registerPageByName(path, name, locale, page);
-
-				return null;
+				omd = editPage.registerPageByTitle(pageEntity.getPath(), 
+						pageEntity.getTitle(), pageEntity.getLocale(), page);
 			}
 			
+			Map<String,Object> md = new HashMap<String,Object>();
+			md.put("path", omd.getPathMetadata().getPath());
+			md.put("id", omd.getPathMetadata().getId());
+			md.put("locale", omd.getLocale());
+			return md;
 		}
 		catch(InvalidRequestException ex) {
 			ex.printStackTrace();
+			throw ex;
+		}
+		catch(ValidatorException ex) {
 			throw ex;
 		}
 		catch(Throwable ex) {
---
+++ src/main/java/br/com/uoutec/community/ediacaran/front/page/pub/PagePubEntity.java
@@ -0,0 +1,223 @@
+package br.com.uoutec.community.ediacaran.front.page.pub;
+
+import java.io.CharArrayReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.brandao.brutos.annotation.Basic;
+import org.brandao.brutos.annotation.ScopeType;
+
+import br.com.uoutec.community.ediacaran.front.components.Image;
+import br.com.uoutec.community.ediacaran.front.page.BreadcrumbPath;
+import br.com.uoutec.community.ediacaran.front.page.EditPage;
+import br.com.uoutec.community.ediacaran.front.page.Page;
+import br.com.uoutec.community.ediacaran.front.page.PageFileManagerHandler;
+import br.com.uoutec.i18n.ValidationException;
+import br.com.uoutec.pub.entity.AbstractPubEntity;
+
+public class PagePubEntity 
+	extends AbstractPubEntity<Page>{
+
+	private static final long serialVersionUID = -796010157890313726L;
+
+	private Long gid;
+	
+	@NotNull
+	@Size(max = 600)
+	@Pattern(regexp="(\\/|" + PageFileManagerHandler.PATH_FORMAT + ")")
+	private String path;
+	
+	@Size(max = 255)
+	@Pattern(regexp=PageFileManagerHandler.ID_FORMAT)
+	private String name;
+	
+	@Pattern(regexp=PageFileManagerHandler.LOCALE_FORMAT)
+	private String locale;
+	
+	@NotNull
+	@Size(max = 255)
+	private String title;
+	
+	@Size(max = 2097152)
+	private String content;
+	
+	private Map<String, String> header;
+	
+	private List<BreadcrumbPath> breadcrumb;
+	
+	private Image thumbnail;
+	
+	@Size(max = 255)
+	private String titleThumbnail;
+	
+	@Size(max = 255)
+	private String shortDescription;
+	
+	@NotNull
+	@Size(max = 120)
+	private String template;
+
+	@Basic(scope=ScopeType.IOC)
+	public EditPage editPage;
+ 
+	public Long getGid() {
+		return gid;
+	}
+
+	public void setGid(Long gid) {
+		this.gid = gid;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocale() {
+		return locale;
+	}
+
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Map<String, String> getHeader() {
+		return header;
+	}
+
+	public void setHeader(Map<String, String> header) {
+		this.header = header;
+	}
+
+	public List<BreadcrumbPath> getBreadcrumb() {
+		return breadcrumb;
+	}
+
+	public void setBreadcrumb(List<BreadcrumbPath> breadcrumb) {
+		this.breadcrumb = breadcrumb;
+	}
+
+	public Image getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(Image thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getTitleThumbnail() {
+		return titleThumbnail;
+	}
+
+	public void setTitleThumbnail(String titleThumbnail) {
+		this.titleThumbnail = titleThumbnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	@Override
+	protected boolean isEqualId(Page instance) throws Throwable {
+		return false;
+	}
+
+	@Override
+	protected boolean hasId(Page instance) throws Throwable {
+		return gid != null;
+	}
+
+	@Override
+	protected Page reloadEntity() throws Throwable {
+		return editPage.getPage(path, locale);
+	}
+
+	@Override
+	protected void throwReloadEntityFail() throws Throwable {
+		throw new ValidationException("page not found!");
+	}
+
+	@Override
+	protected Page createNewInstance() throws Throwable {
+		return new Page();
+	}
+
+	@Override
+	protected void copyTo(Page o, boolean reload, boolean override, boolean validate) throws Throwable {
+		
+		o.setBreadcrumb(breadcrumb);
+		o.setHeader(header);
+		o.setThumbnailDescription(shortDescription);
+		o.setThumbnailTitle(titleThumbnail);
+		o.setThumbnail(thumbnail == null? null : thumbnail.save(560, 292));
+		o.setTemplate(template);
+		o.setTitle(title);
+		o.setContent(content == null? null : new CharArrayReader(content.toCharArray()));
+		
+	}
+
+	protected void validate(Class<?> ... groups) throws ValidationException{
+		super.validate(groups);
+		
+		if(gid != null) {
+			Map<String,Object> md = new HashMap<String,Object>();
+			md.put("path", path);
+			md.put("id", name);
+			md.put("locale", locale);
+
+			if(gid != md.hashCode()) {
+				throw new ValidationException("invalid gid");
+			}
+			
+		}
+		if(!editPage.isValidTemplate(template)) {
+			throw new ValidationException("invalid template");
+		}
+		
+	}
+	
+}
