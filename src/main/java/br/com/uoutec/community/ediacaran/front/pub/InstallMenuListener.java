4265809c33649f22e95348b5ae89f9b492755940
Brandao <Brandao@git.uoutec.com.br>
2024-03-20 10:31:29 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/front/pub/InstallMenuListener.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/pub/InstallMenuListener.java
@@ -10,6 +10,8 @@ import br.com.uoutec.community.ediacaran.system.repository.PathMetadata;
 public class InstallMenuListener 
 	implements ObjectsManagerDriverListener {
 
+	private final Object globalID;
+	
 	private final String path;
 	
 	private final String id;
@@ -21,13 +23,15 @@ public class InstallMenuListener
 		this.path = builder.path;
 		this.id = builder.id;
 		this.menus = builder.menus;
+		this.globalID = builder.globalID;
 	}
 	
-	public InstallMenuListener(String path, String id, IMenusParams menus) {
+	public InstallMenuListener(Object globalID, String path, String id, IMenusParams menus) {
 		super();
 		this.path = path;
 		this.id = id;
 		this.menus = menus;
+		this.globalID = globalID;
 	}
 
 	public void afterLoad(ObjectMetadata omd, ObjectValue obj) {
@@ -48,19 +52,47 @@ public class InstallMenuListener
 		
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((globalID == null) ? 0 : globalID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InstallMenuListener other = (InstallMenuListener) obj;
+		if (globalID == null) {
+			if (other.globalID != null)
+				return false;
+		} else if (!globalID.equals(other.globalID))
+			return false;
+		return true;
+	}
+
 	public interface IMenusParams {
-		
 		void set(MenuBar value);
-		
 	}
 	
 	@Generated("SparkTools")
-	public static IPathStage builder() {
+	public static IGlobalIDStage builder() {
 		return new Builder();
 	}
 
 	@Generated("SparkTools")
-	public interface IPathStage {
+	public interface IGlobalIDStage {
+		public IPathStage withGlobalID(Object globalID);
+	}
+	
+	@Generated("SparkTools")
+	public interface IPathStage extends IBuildStage {
 		public IIdStage withPath(String path);
 	}
 
@@ -77,18 +109,23 @@ public class InstallMenuListener
 	@Generated("SparkTools")
 	public interface IBuildStage {
 		public InstallMenuListener build();
-		
 	}
 
 	@Generated("SparkTools")
-	public static final class Builder implements IPathStage, IMenusStage, IIdStage, IBuildStage {
+	public static final class Builder implements IPathStage, IGlobalIDStage, IMenusStage, IIdStage, IBuildStage {
 		private String path;
 		private String id;
+		private Object globalID;
 		private IMenusParams menus;
 
 		private Builder() {
 		}
 
+		public IPathStage withGlobalID(Object globalID) {
+			this.globalID = globalID;
+			return this;
+		}
+		
 		@Override
 		public IIdStage withPath(String path) {
 			this.path = path;
