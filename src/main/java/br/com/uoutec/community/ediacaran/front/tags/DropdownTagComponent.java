cfa0ea37f376e074db1b614f1268a832877bd08e
Brandao <Brandao@git.uoutec.com.br>
2024-05-01 16:31:38 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/front/tags/DropdownTagComponent.java
+++ src/main/java/br/com/uoutec/community/ediacaran/front/tags/DropdownTagComponent.java
@@ -9,7 +9,7 @@ import br.com.uoutec.community.ediacaran.front.tags.doc.TagAttribute;
 	uri="https://www.uoutec.com.br/ediacaran/tags/components", 
 	bodycontent=BodyTypes.SCRIPTLESS
 )
-public class DropdownTagComponent  extends FieldFormTagComponent {
+public class DropdownTagComponent  extends AbstractSimpleTagComponent {
 
 	public static final String TEMPLATE  = "/components/dropdown";
 	
